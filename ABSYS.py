0dff583a5e8df8e4fc6d6d79f43196c391a0d1be
pulkitjain1821 <pulkitjain1821@gmail.com>
2013-01-29 20:32:55 +0530

    ABSYS started

---
+++ ABSYS.py
@@ -0,0 +1,13 @@
+import sys;
+
+t = int(raw_input());
+for i in range (0,t):
+    x,y,z = map(int,sys.stdin.readline().split())
+    n = int((2*z)/(x+y))
+    d = (y-x)/(n-5)
+    a = x-(2*d)
+    print n
+    for j in range(0,n):
+        sys.stdout.write(a+j*d)
+        sys.stdout.write(' ')
+    print' '
