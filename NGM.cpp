2d1a46aa073ed264bd44a6c35d8e674cc0faa536
pulkitjain1821 <pulkitjain1821@gmail.com>
2013-08-06 20:50:54 +0530

    Create NGM.cpp

---
+++ NGM.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+  int n;
+	scanf("%d",&n);
+	if(n % 10 == 0)
+		printf("2\n");
+	else
+		printf("1\n%d\n",n % 10);
+	return 0;
+}
