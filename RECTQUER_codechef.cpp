5b4ee5824b80381f2d36683e037814e4c48ed43d
pulkitjain1821 <pulkitjain1821@gmail.com>
2013-12-17 21:09:20 +0530

    Update RECTQUER_codechef.cpp

--- RECTQUER_codechef.cpp
+++ RECTQUER_codechef.cpp
@@ -1,19 +1,48 @@
 #include<cstdio>
 #include<cstdlib>
 using namespace std;
-
-int count[301][301][10];
+ 
+unsigned short count[301][301][10];
 int arr[300][300];
+ 
+int readInt () 
+{
+        bool minus = false;
+        int result = 0;
+        char ch;
+        ch = getchar_unlocked();
+        //ch = getchar();
+        while (true) {
+                if (ch == '-') break;
+                if (ch >= '0' && ch <= '9') break;
+                //ch = getchar();
+                ch = getchar_unlocked();
+        }
+        if (ch == '-') minus = true; else result = ch-'0';
+        while (true) {
+                ch = getchar_unlocked();
+                //ch = getchar();
+                if (ch < '0' || ch > '9') break;
+                result = result*10 + (ch - '0');
+        }
+        if (minus)
+                return -result;
+        else
+                return result;
+}
+ 
 int main()
 {
 	int n;
-	scanf("%d",&n);
+	n = readInt();
+	//scanf("%d",&n);
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n;j++)
 		{
-			scanf("%d",*(arr+i)+j);
-			arr[i][j]--;
+			//scanf("%d",*(arr+i)+j);
+			//arr[i][j]--;
+			arr[i][j] = readInt() -1;
 		}
 	}
 	
@@ -21,7 +50,7 @@ int main()
 	{
 		count[i+1][1][arr[i][0]]++;
 	}
-
+ 
 	for(int i=0;i<n;i++)
 	{
 		for(int j=1;j<n;j++)
@@ -33,7 +62,7 @@ int main()
 			count[i+1][j+1][arr[i][j]]++;
 		}
 	}
-
+ 
 	for(int j=0;j<n;j++)
 	{
 		for(int i=1;i<n;i++)
@@ -44,7 +73,7 @@ int main()
 			}
 		}
 	}
-
+ 
 	/*for(int k=0;k<10;k++)
 	{
 		for(int i=0;i<n;i++)
@@ -63,8 +92,13 @@ int main()
 	
 	for(int i=0;i<q;i++)
 	{
-		scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+		//scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
 		//x1--;x2--;y1--;y2--;
+		
+		x1 = readInt();
+		y1 = readInt();
+		x2 = readInt();
+		y2 = readInt();
 		x1--;y1--;
 		int ans =0;
 		for(int k=0;k<10;k++)
@@ -78,4 +112,4 @@ int main()
 		printf("%d\n",ans);
 	}
 	return 0;
-}
+} 
