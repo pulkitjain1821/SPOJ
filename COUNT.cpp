621478df99b53d3764ef86467ce554f7efaebfeb
pulkitjain1821 <pulkitjain1821@gmail.com>
2013-12-22 19:06:22 +0530

    Create COUNT.cpp

---
+++ COUNT.cpp
@@ -0,0 +1,48 @@
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+long long int ans[5001][5001];
+int main()
+{
+	for(int i=0;i<=5000;i++)
+	{
+
+		ans[0][i] = 1;
+	}
+	
+	for(int i=1;i<=5000;i++)
+	{
+		for(int j=1;j<=5000;j++)
+		{
+		/*	int k = 0;
+			while(k<=i)
+			{
+				ans[i][j] = (ans[i][j] + ans[k][j-1])%1988;
+				k++;
+			}
+		*/
+			ans[i][j] = ans[i][j-1] ;
+			if(i-j>=0)
+			ans[i][j] =	(ans[i][j] + ans[i-j][j])%1988;
+		}
+	}
+	
+	/*for(int z=0;z<=4;z++)
+	{
+		for(int l=0;l<=4;l++)
+		{
+			printf("%d ",ans[z][l]);
+		}
+		printf("\n");
+	}
+	*/
+	int n,k;
+	while(true)
+	{
+		scanf("%d%d",&n,&k);
+		if(n==0 && k==0)
+			break;
+		printf("%d\n",ans[n-k][k]);
+	}
+	return 0;
+}
