a6dfbd5d57c65df5e556e53fae68f25c4fbb2272
pulkitjain1821 <pulkitjain1821@gmail.com>
2014-01-02 18:35:59 +0530

    Create readInt.cpp

---
+++ readInt.cpp
@@ -0,0 +1,25 @@
+/*function for fast input ouput. Not my own but got somewhere from codeforces.*/
+
+
+int readInt () 
+{
+	bool minus = false;
+	int result = 0;
+	char ch;
+	ch = getchar_unlocked();
+	while (true) {
+		if (ch == '-') break;
+		if (ch >= '0' && ch <= '9') break;
+		ch = getchar_unlocked();
+	}
+	if (ch == '-') minus = true; else result = ch-'0';
+	while (true) {
+		ch = getchar_unlocked();
+		if (ch < '0' || ch > '9') break;
+		result = result*10 + (ch - '0');
+	}
+	if (minus)
+		return -result;
+	else
+		return result;
+}
